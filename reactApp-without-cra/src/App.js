668685c78b78270d7ffbb14aa42adb32c3991add
ahsankhan1911 <ahsankhan1911@gmail.com>
2018-07-13 17:44:10 +0500

    added react client

---
+++ reactApp-without-cra/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+    render () {
+        return (
+            <div>hello worlddddsdsd</div>
+        )
+    }
+}
+
+export default App;
---
+++ reactApp-without-cra/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: { path:__dirname, filename: 'bundle.js',
+    publicPath : path.resolve('public')
+},
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // include .jsx files
+                exclude: /node_modules/, // exclude any and all files in the node_modules folder
+                loader: "babel-loader",
+            },
+            {
+              test: /\.css$/,
+              use: ["style-loader", "css-loader"]
+            }
+
+        ],
+
+     
+    }
+}
